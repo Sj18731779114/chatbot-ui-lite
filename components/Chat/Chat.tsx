7649592b25e20b38dc05df5e526c4434f20ed735
songbb <992073172@qq.com>
2025-03-26 17:22:23 +0800

    聊天窗口优化..

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -5,36 +5,39 @@ import { ChatLoader } from "./ChatLoader";
 import { ChatMessage } from "./ChatMessage";
 
 interface Props {
-  messages: Message[];
-  loading: boolean;
-  onSend: (message: Message) => void;
-  onReset: () => void;
-  onAbort: () => void;
+    messages: Message[];
+    loading: boolean;
+    onSend: (message: Message) => void;
+    onReset: () => void;
+    onAbort: () => void;
 }
 
 export const Chat: FC<Props> = ({ messages, loading, onSend, onReset, onAbort }) => {
-  return (
-      <>
-        <div className="flex flex-col rounded-lg px-2 sm:p-4 sm:border border-neutral-300">
-          {messages.map((message, index) => (
-              <div
-                  key={index}
-                  className="my-1 sm:my-1.5"
-              >
-                <ChatMessage message={message} />
-              </div>
-          ))}
+    return (
+        <>
+            <div className="bottom-0 left-0 w-full">
+                <div className="flex flex-col rounded-lg px-2 sm:p-4 sm:border border-neutral-300">
+                    {messages.map((message, index) => (
+                        <div
+                            key={index}
+                            className="my-1 sm:my-1.5"
+                        >
+                            <ChatMessage message={message} />
+                        </div>
+                    ))}
 
-          {loading && (
-              <div className="my-1 sm:my-1.5">
-                <ChatLoader />
-              </div>
-          )}
+                    {loading && (
+                        <div className="my-1 sm:my-1.5">
+                            <ChatLoader />
+                        </div>
+                    )}
+                </div>
 
-          <div className="mt-4 sm:mt-8 bottom-[56px] left-0 w-full">
-            <ChatInput onSend={onSend} />
-          </div>
-        </div>
-      </>
-  );
+                <div className="fixed flex-1 w-full max-w-[800px] mx-auto mt-2 px-3 sm:px-0 bottom-0"
+                     style={{ boxShadow: "0 -20px 180px rgba(0, 0, 0, 0.085)"}}>
+                    <ChatInput onSend={onSend} />
+                </div>
+            </div>
+        </>
+    );
 };
